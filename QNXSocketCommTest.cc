8b6df1f55298b5555481736d4bfc58bdf7203b61
sunbiao <632058099@qq.com>
2017-11-21 17:29:49 +0800

    update files

--- QNXSocketCommTest.cc
+++ QNXSocketCommTest.cc
@@ -11,75 +11,216 @@
 #include <thread>
 
 #include "CSocketBase.h"
+#include "CSocketServer.h"
+#include "CSocketImpl.h"
 
+
+static struct timespec StartTimeStamp;
 int send_test(int id)
 {
 	sleep(1);
-	CSocketBase* pObj = GetSocketCommSingelObject();
+	CIPCBase* pObj = GetIPCSingelObject();
 	pObj->Create("server_socket", 0);
 	pObj->RegisterReceiveDataCallBack([](int appid, char *pData, int len){
-		printf("Get data From Server:%s\n", pData);
+		printf("0 Get data From Server:%s\n", pData);
 	});
 	char buf[32];
 	int i = 0;
-	while(1)
+	while(i<2)
 	{
 		snprintf(buf, 31, "data[%d]", i++);
+		clock_gettime(CLOCK_REALTIME, &StartTimeStamp);
 		pObj->Send(buf, sizeof(buf), 0);
-		sleep(1);
+		usleep(1000*1000);
 	}
+	pObj->Destroy();
 }
 
 
 int receive()
 {
-	  /* delete the socket file */
-	  unlink("server_socket");
-
-	  /* create a socket */
-	  int server_sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-
-	  struct sockaddr_un server_addr;
-	  server_addr.sun_family = AF_UNIX;
-	  strcpy(server_addr.sun_path, "server_socket");
-
-	  /* bind with the local file */
-	  bind(server_sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-	  /* listen */
-	  listen(server_sockfd, 5);
-
-	  int client_sockfd;
-	  struct sockaddr_un client_addr;
-	  socklen_t len = sizeof(client_addr);
-	  while(1)
-	  {
-			/* accept a connection */
-			client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_addr, &len);
-		    printf("server out waiting:\n\n");
-			std::thread([client_sockfd](){
-				char buf[32];
-				T_SM_FIRST_PACKET packet;
-				while(1)
-				{
-					read(client_sockfd, &packet, sizeof(packet));
-					read(client_sockfd, buf, packet.dataLen);
-					printf("get data from client,data[%s]\n", buf);
-
-					write(client_sockfd, &packet, sizeof(packet));
-					write(client_sockfd, buf, packet.dataLen);
-				}
-			}).detach();
-	  }
-
-	  return 0;
+	CIPCBase* pObj = GetIPCServerSingelObject();
+	pObj->Create("server_socket", 0);
 }
 
 int main(int argc, char *argv[])
 {
 	std::cout << "Welcome to the QNX Momentics IDE" << std::endl;
 	std::thread([](){
-		send_test(0);
+
+		sleep(1);
+		CIPCBase* pObj = new CSocketImpl();
+		pObj->Create("server_socket", 1);
+		pObj->RegisterReceiveDataCallBack([](int appid, char *pData, int len){
+			struct timespec endTimeStamp;
+			clock_gettime(CLOCK_REALTIME, &endTimeStamp);
+			printf("1 Get data From Server:%s timestamp[%ld ns]\n", pData, (endTimeStamp.tv_nsec - StartTimeStamp.tv_nsec));
+		});
+
+		char buf[32];
+		int i = 0;
+		while(i<2)
+		{
+			snprintf(buf, 31, "data[%d]", i++);
+			clock_gettime(CLOCK_REALTIME, &StartTimeStamp);
+			pObj->Send(buf, sizeof(buf), 0);
+			usleep(1000*1000);
+		}
+		pause();
+
+
+	}
+			).detach();
+
+
+	std::thread([](){
+
+		sleep(1);
+		CIPCBase* pObj = new CSocketImpl();
+		pObj->Create("server_socket", 2);
+		pObj->RegisterReceiveDataCallBack([](int appid, char *pData, int len){
+			struct timespec endTimeStamp;
+			clock_gettime(CLOCK_REALTIME, &endTimeStamp);
+			printf("1 Get data From Server:%s timestamp[%ld ns]\n", pData, (endTimeStamp.tv_nsec - StartTimeStamp.tv_nsec));
+		});
+
+		char buf[32];
+		int i = 0;
+		while(i<2)
+		{
+			snprintf(buf, 31, "data[%d]", i++);
+			clock_gettime(CLOCK_REALTIME, &StartTimeStamp);
+			pObj->Send(buf, sizeof(buf), 0);
+			usleep(1000*1000);
+		}
+		pause();
+
+	}
+			).detach();
+
+	std::thread([](){
+
+		sleep(1);
+		CIPCBase* pObj = new CSocketImpl();
+		pObj->Create("server_socket", 3);
+		pObj->RegisterReceiveDataCallBack([](int appid, char *pData, int len){
+			struct timespec endTimeStamp;
+			clock_gettime(CLOCK_REALTIME, &endTimeStamp);
+			printf("1 Get data From Server:%s timestamp[%ld ns]\n", pData, (endTimeStamp.tv_nsec - StartTimeStamp.tv_nsec));
+		});
+
+		char buf[32];
+		int i = 0;
+		while(i<2)
+		{
+			snprintf(buf, 31, "data[%d]", i++);
+			clock_gettime(CLOCK_REALTIME, &StartTimeStamp);
+			pObj->Send(buf, sizeof(buf), 0);
+			usleep(1000*1000);
+		}
+		pause();
+
+	}
+			).detach();
+
+
+	std::thread([](){
+
+		sleep(1);
+		CIPCBase* pObj = new CSocketImpl();
+		pObj->Create("server_socket", 4);
+		pObj->RegisterReceiveDataCallBack([](int appid, char *pData, int len){
+			struct timespec endTimeStamp;
+			clock_gettime(CLOCK_REALTIME, &endTimeStamp);
+			printf("1 Get data From Server:%s timestamp[%ld ns]\n", pData, (endTimeStamp.tv_nsec - StartTimeStamp.tv_nsec));
+		});
+
+		char buf[32];
+		int i = 0;
+		while(i<2)
+		{
+			snprintf(buf, 31, "data[%d]", i++);
+			clock_gettime(CLOCK_REALTIME, &StartTimeStamp);
+			pObj->Send(buf, sizeof(buf), 0);
+			usleep(1000*1000);
+		}
+		pause();
+
+	}
+			).detach();
+
+
+	std::thread([](){
+
+		sleep(1);
+		CIPCBase* pObj = new CSocketImpl();
+		pObj->Create("server_socket", 5);
+		pObj->RegisterReceiveDataCallBack([](int appid, char *pData, int len){
+			struct timespec endTimeStamp;
+			clock_gettime(CLOCK_REALTIME, &endTimeStamp);
+			printf("1 Get data From Server:%s timestamp[%ld ns]\n", pData, (endTimeStamp.tv_nsec - StartTimeStamp.tv_nsec));
+		});
+		char buf[32];
+		int i = 0;
+		while(i<2)
+		{
+			snprintf(buf, 31, "data[%d]", i++);
+			clock_gettime(CLOCK_REALTIME, &StartTimeStamp);
+			pObj->Send(buf, sizeof(buf), 0);
+			usleep(1000*1000);
+		}
+		pause();
+
+	}
+			).detach();
+
+
+	std::thread([](){
+
+		sleep(1);
+		CIPCBase* pObj = new CSocketImpl();
+		pObj->Create("server_socket", 6);
+		pObj->RegisterReceiveDataCallBack([](int appid, char *pData, int len){
+			struct timespec endTimeStamp;
+			clock_gettime(CLOCK_REALTIME, &endTimeStamp);
+			printf("1 Get data From Server:%s timestamp[%ld ns]\n", pData, (endTimeStamp.tv_nsec - StartTimeStamp.tv_nsec));
+		});
+		char buf[32];
+		int i = 0;
+		while(i<2)
+		{
+			snprintf(buf, 31, "data[%d]", i++);
+			clock_gettime(CLOCK_REALTIME, &StartTimeStamp);
+			pObj->Send(buf, sizeof(buf), 0);
+			usleep(1000*1000);
+		}
+		pause();
+
+	}
+			).detach();
+
+
+	std::thread([](){
+
+		sleep(1);
+		CIPCBase* pObj = new CSocketImpl();
+		pObj->Create("server_socket", 7);
+		pObj->RegisterReceiveDataCallBack([](int appid, char *pData, int len){
+			struct timespec endTimeStamp;
+			clock_gettime(CLOCK_REALTIME, &endTimeStamp);
+			printf("1 Get data From Server:%s timestamp[%ld ns]\n", pData, (endTimeStamp.tv_nsec - StartTimeStamp.tv_nsec));
+		});
+		char buf[32];
+		int i = 0;
+		while(i<2)
+		{
+			snprintf(buf, 31, "data[%d]", i++);
+			clock_gettime(CLOCK_REALTIME, &StartTimeStamp);
+			pObj->Send(buf, sizeof(buf), 0);
+			usleep(1000*1000);
+		}
+		pause();
+
 	}
 			).detach();
 
@@ -89,5 +230,7 @@ int main(int argc, char *argv[])
 			).detach();
 
 	receive();
+
+	pause();
 	return EXIT_SUCCESS;
 }
